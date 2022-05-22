16595bea86944bf2eb597b24fd50ec52b8882880
FestusCode <festusdjossou@gmail.com>
2022-05-23 00:51:52 +0100

    browser router deleted to fix deployement bug

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
   import Home from './Components/Home';
   import Banner from './Components/Banner'
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { HashRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
-      <BrowserRouter>
+      <HashRouter>
       <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/banner" element={<Banner/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
